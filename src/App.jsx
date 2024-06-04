5816ef2a82cda93ee75380f085ece759dbf964cb
Toshita benale <toshitabendale5@gmail.com>
2024-06-04 19:31:41 +0530

    lifter up the state to app component .Show status message to the user ,calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import './styles.scss';
+import { useState } from 'react';
 import Board from './components/Board';
+import { calculatewinner } from './winner';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculatewinner(squares);
+  const nextPlayer = isXNext ? 'x' : 'O';
+  const statusMessage = winner
+    ? `winner is  ${winner}`
+    : `Next Player is ${nextPlayer}`;
+
+  const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition] || winner) {
+      return;
+    }
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+
+    setIsXNext(currentIsXNext => !currentIsXNext);
+  };
+
   return (
     <div className="app">
-      <Board />
+      <h2>{statusMessage}</h2>
+      <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
